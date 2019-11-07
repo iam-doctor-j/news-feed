05a012b6901a508f177c043a62d41198aa5e76c4
Jyotirmaya Sahu <jyotirmayasahu38@gmail.com>
2019-11-07 23:33:16 +0530

    added image preview

--- src/app/posts-loader/new-post/new-post.component.ts
+++ src/app/posts-loader/new-post/new-post.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-new-post',
-  templateUrl: './new-post.component.html',
-  styleUrls: ['./new-post.component.css']
+  selector: "app-new-post",
+  templateUrl: "./new-post.component.html",
+  styleUrls: ["./new-post.component.css"]
 })
 export class NewPostComponent implements OnInit {
   // @ViewChild('f', { static: false }) postForm: NgForm;
   isEdit = false;
   postRow = 2;
-  photoUrl: string;
+  imgUrl: any;
+  imagePath: string;
 
   constructor() {}
 
@@ -24,10 +25,26 @@ export class NewPostComponent implements OnInit {
   onPostEditClose() {
     this.isEdit = false;
     this.postRow = 2;
+    this.imgUrl = null;
+    this.imagePath = null;
+  }
+
+  onImageChange(event) {
+    const files = event.target.files;
+    if (files.length === 0) return;
+    let reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgUrl = reader.result;
+    }
+    console.log(this.imagePath);
+    console.log(this.imgUrl);
+    this.onPostEdit();
   }
 
   onSubmit(postForm: NgForm) {
     console.log(postForm);
-    this.photoUrl = postForm.form.value.postImg;
+    // this.photoUrl = postForm.form.value.postImg;
   }
 }
